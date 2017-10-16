324748cb08fb99154e697a87a1ee4e193eaf57a7
AkMedIm <mohamedakdim1@gmail.com>
2017-10-16 14:02:15 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+(function musicDB() {
+  this.init = function(){
+    this.search();
+  };
 
+  this.search = function(){
+    var form = document.querySelector("#form");
+    form.addEventListener("submit",function(e){
+      e.preventDefault();
+      var value = document.querySelector("#input_search").value;
+      form.reset();
+
+      getData(value);
+
+    });
+  };
+  this.getData = function(artist){
+    var http= new XMLHttpRequest();
+    var url = "https://itunes.apple.com/search?term=audioslave&entity=album";
+  };
+  this.init();
+}) ();
